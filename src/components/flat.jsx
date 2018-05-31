e521ed8cc17e34a9573e0c5bc8b133b3cbaa848d
marci papineau <marcidenmark@hotmail.com>
2018-05-31 10:14:46 +0200

    rendering a static flat– using a flat component and a flat_list container

---
+++ src/components/flat.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Flat extends Component {
+	render () {
+		const style = {
+			// background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('https://kitt.lewagon.com/placeholder/cities/shanghai'
+			// backgroundImage: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('https://kitt.lewagon.com/placeholder/cities/shanghai'
+			backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${this.props.flat.imageUrl}')`
+
+		}
+
+		return (
+		<div className="card flat" style={style} >
+			<div className="card-description">
+			    <h2>{this.props.flat.name}</h2>
+			    <p>{this.props.flat.price} {this.props.flat.priceCurrency}</p>
+			</div>
+		</div>
+
+		);
+	}
+
+}
+
+export default Flat;
--- src/containers/flat_list.jsx
+++ src/containers/flat_list.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 
+import Flat from '../components/flat';
+
 class FlatList extends Component {
-	render () {
+  // TEMPORARY CODE TO INTEGRATE HTML
+	static defaultProps = {
+		flats: [{
+	     	"name": "Charm at the Steps of Montmartre",
+	     	"imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+	      	"price": 164,
+	      	"priceCurrency": "EUR"
+    		}]
+	}
+
+	render() {
 		return (
-		<div className="flat-list col-sm-7">
-		<p>
-			dsafdsfad
-		</p>
-		</div>
-		);
+			<div className="flat-list col-sm-7">
+				{this.props.flats.map((flat) => <Flat flat={flat} key={flat.name} />)}
+			</div>
+
+			);
 	}
 }
+
+// 	render () {
+// 		return (
+// 		<div className="flat-list col-sm-7">
+// 		<p>
+// 			dsafdsfad
+// 		</p>
+// 		</div>
+// 		);
+// 	}
+// }
 
 export default FlatList;
